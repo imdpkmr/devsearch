f03a4a0fb4a294fe06919bf4797833b6795ad2d9
deepak <kdeepak2912@gmail.com>
2021-12-18 23:14:47 +0530

    37. Sending POST Requests; without JWT, facing issue with Authentication credentials were not provided.

--- api/views.py
+++ api/views.py
@@ -3,8 +3,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from .serializers import ProjectSerializer
-from projects.models import Project
-
+from projects.models import Project, Review
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -12,7 +11,7 @@ def getRoutes(request):
         {'GET':'/api/projects/'},
         {'GET':'/api/projects/id'},
         {'POST':'/api/projects/id/vote'},
-        
+
         {'POST':'/api/users/token'},
         {'POST':'/api/users/token/refresh'},
 
@@ -31,5 +30,24 @@ def getProjects(request):
 @api_view(['GET'])
 def getProject(request, pk):
     project = Project.objects.get(id=pk)
+    serializer = ProjectSerializer(project, many=False)
+    return Response(serializer.data)
+
+@api_view(['POST', ])
+# @permission_classes([IsAuthenticated])
+def projectVote(request, pk):
+    project = Project.objects.get(id=pk)
+    user = request.user.profile
+    data = request.data
+
+    review, created = Review.objects.get_or_create(
+        owner=user,
+        project=project,
+    )
+
+    review.value = data['value']
+    review.save()
+    project.getVoteCount
+
     serializer = ProjectSerializer(project, many=False)
     return Response(serializer.data)
